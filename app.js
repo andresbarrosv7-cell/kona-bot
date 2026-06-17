9ffaad9ae5aa33dbf596fbf46136566a6c4714b8
andresbarrosv7-cell <andresbarrosv7@gmail.com>
2026-06-17 15:54:24 -0500

    Debug webhook

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ app.use(express.static("public"));
 const VERIFY_TOKEN = "kona_verify_2026";
 
 // WEBHOOK META
-
+console.log(
+  "🔥 WEBHOOK POST RECIBIDO",
+  JSON.stringify(req.body, null, 2)
+);
 app.get("/webhook", (req, res) => {
 
 const mode = req.query["hub.mode"];
